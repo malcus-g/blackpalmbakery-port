251950c5bf186eb88514068d785df0856dce91c7
hakther15 <92819734+hakther15@users.noreply.github.com>
2025-03-26 21:00:44 -0400

    added image and headers to home page
    made width changes to section-divider

--- ui/section-divider.tsx
+++ ui/section-divider.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion'
 export default function SectionDivider() {
   return (
     <motion.div 
-    className="bg-gray-300 my-12 h-1 w-24 rounded-full hidden sm:block justify-center"
+    className="bg-gray-300 my-12 h-1 w-150 rounded-full hidden sm:block justify-center"
     initial={{ opacity: 0, x: 100 }}
     animate={{ opacity: 1, x: 0 }}
     transition= {{

96712fcb4f2213170232764e82f53bbdb2da2bde
Garrett <garrettcouch97@gmail.com>
2025-03-27 12:43:57 -0400

    add fields to contact form

--- ui/contact/contact-form.tsx
+++ ui/contact/contact-form.tsx
@@ -9,7 +9,7 @@ import { sendEmail } from '@/actions/sendEmail';
 export default function ContactForm() {
   return (
     <motion.form
-        className="flex flex-col w-full sm:w-[38rem]"
+        className="flex flex-col w-full gap-10 sm:w-[38rem]"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1 }}
@@ -22,7 +22,7 @@ export default function ContactForm() {
           toast.success("Email successfully sent!");
       }}
     >
-        <div className="flex flex-col gap-10 mb-16">
+        <div className="flex flex-col gap-10 mb-14">
           <p>
             After you fill out this form, I will contact you to go over details regarding your needs. Please allow 24 hours for response. 
           </p>
@@ -30,6 +30,45 @@ export default function ContactForm() {
             THIS FORM DOES NOT CONFIRM ANY ORDER(S).
           </p>
         </div>
+        <fieldset className="flex gap-10">
+          <legend className="mb-6">Are you a new or existing customer?</legend>
+          <div className="flex flex-row gap-4">
+            <input
+                type="radio"
+                name="prevCustomer"
+                value="new"
+                required
+                className="h-6 w-6"
+            />
+            <label htmlFor="new">I am a new customer</label>
+          </div>
+          <div className="flex flex-row gap-4">
+            <input
+                type="radio"
+                name="prevCustomer"
+                value="existing"
+                required
+                className="h-6 w-6"
+            />
+            <label htmlFor="new">I am an existing customer</label>
+          </div>
+        </fieldset>
+        <input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            required
+            maxLength={100}
+            className="h-14 px-4 rounded-lg borderBlack transition-all bg-white"
+        />
+        <input
+            type="tel"
+            name="phone"
+            placeholder="Your Phone Number"
+            required
+            maxLength={15}
+            className="h-14 px-4 rounded-lg borderBlack transition-all bg-white"
+        />
         <input 
             type="email"
             name="senderEmail" 
@@ -38,13 +77,35 @@ export default function ContactForm() {
             required
             maxLength={500}
         />
-
+        <fieldset className="flex gap-10">
+          <legend className="mb-6">Preferred contact method:</legend>
+          <div className="flex flex-row gap-4">
+            <input
+                type="radio"
+                name="contactMethod"
+                value="phone"
+                required
+                className="h-6 w-6"
+            />
+            <label htmlFor="new">Phone</label>
+          </div>
+          <div className="flex flex-row gap-4">
+            <input
+                type="radio"
+                name="contactMethod"
+                value="email"
+                required
+                className="h-6 w-6"
+            />
+            <label htmlFor="new">Email</label>
+          </div>
+        </fieldset>
         <textarea 
-            placeholder="Your Message"
+            placeholder="Questions or comments?"
             name="message"
             required
             maxLength={5000}
-            className="h-52 my-3 px-4 rounded-lg borderBlack p-4 transition-all bg-white" 
+            className="h-52 rounded-lg borderBlack p-4 transition-all bg-white" 
         />
         <SubmitBtn />
     </motion.form>

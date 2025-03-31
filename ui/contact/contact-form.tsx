ad40ef960ad413871a021e932ee942e3d815227f
Garrett <garrettcouch97@gmail.com>
2025-03-31 09:45:36 -0400

    add labels to contact form fields

--- ui/contact/contact-form.tsx
+++ ui/contact/contact-form.tsx
@@ -22,7 +22,7 @@ export default function ContactForm() {
           toast.success("Email successfully sent!");
       }}
     >
-        <div className="flex flex-col gap-10 mb-14">
+        <div className="flex flex-col gap-10 mb-10">
           <p>
             After you fill out this form, I will contact you to go over details regarding your needs. Please allow 24 hours for response. 
           </p>
@@ -30,8 +30,8 @@ export default function ContactForm() {
             THIS FORM DOES NOT CONFIRM ANY ORDER(S).
           </p>
         </div>
-        <fieldset className="flex gap-10">
-          <legend className="mb-6">Are you a new or existing customer?</legend>
+        <fieldset className="flex gap-12">
+          <legend className="mb-8">Are you a new or existing customer?</legend>
           <div className="flex flex-row gap-4">
             <input
                 type="radio"
@@ -53,30 +53,39 @@ export default function ContactForm() {
             <label htmlFor="existing">I am an existing customer</label>
           </div>
         </fieldset>
-        <input
-            type="text"
-            name="senderName"
-            placeholder="Your Name"
-            required
-            maxLength={100}
-            className="h-14 px-4 rounded-lg borderBlack transition-all bg-white"
-        />
-        <input
-            type="tel"
-            name="senderPhone"
-            placeholder="Your Phone Number"
-            required
-            maxLength={15}
-            className="h-14 px-4 rounded-lg borderBlack transition-all bg-white"
-        />
-        <input 
-            type="email"
-            name="senderEmail" 
-            className="h-14 px-4 rounded-lg borderBlack transition-all bg-white" 
-            placeholder="Your Email"
-            required
-            maxLength={500}
-        />
+        <div className="flex flex-col gap-4 text-left">
+          <label htmlFor="senderName">First & Last Name:</label>
+          <input
+              type="text"
+              name="senderName"
+              placeholder="Your Name"
+              required
+              maxLength={100}
+              className="h-14 px-4 rounded-lg borderBlack transition-all bg-white"
+          />
+        </div>
+        <div className="flex flex-col gap-4 text-left">
+          <label htmlFor="senderPhone">Phone Number:</label>
+          <input
+              type="tel"
+              name="senderPhone"
+              placeholder="123-456-7890"
+              required
+              maxLength={15}
+              className="h-14 px-4 rounded-lg borderBlack transition-all bg-white"
+          />
+        </div>
+        <div className="flex flex-col gap-4 text-left">
+          <label htmlFor="senderEmail">Email:</label>
+          <input 
+              type="email"
+              name="senderEmail" 
+              className="h-14 px-4 rounded-lg borderBlack transition-all bg-white" 
+              placeholder="Your Email"
+              required
+              maxLength={500}
+          />
+        </div>
         <fieldset className="flex gap-10">
           <legend className="mb-6">Preferred contact method:</legend>
           <div className="flex flex-row gap-4">
@@ -100,13 +109,16 @@ export default function ContactForm() {
             <label htmlFor="new">Email</label>
           </div>
         </fieldset>
-        <textarea 
-            placeholder="Questions or comments"
-            name="message"
-            required
-            maxLength={5000}
-            className="h-52 rounded-lg borderBlack p-4 transition-all bg-white" 
-        />
+        <div className="flex flex-col gap-4 text-left">
+          <label htmlFor="message">Questions or Comments:</label>
+          <textarea 
+              placeholder="Please provide details about your request..."
+              name="message"
+              required
+              maxLength={5000}
+              className="h-52 rounded-lg borderBlack p-4 transition-all bg-white" 
+          />
+        </div>
         <SubmitBtn />
     </motion.form>
   )

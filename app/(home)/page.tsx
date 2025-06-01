64f6b7d9775ed057b6158176f829c5b8dd71f639
Garrett <149735110+malcus-g@users.noreply.github.com>
2025-06-01 11:56:26 -0400

    remove aloha lou flyer

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,14 +4,12 @@ import Gallery from "@/ui/gallery/gallery";
 import Contact from "@/ui/contact/contact";
 import About from "@/ui/about/about";
 import Menu from "@/ui/menu/menu";
-import Flyer from "@/ui/flyer";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-20 sm:gap-30 md:gap-40">
       <Welcome />
       <Services />
-      <Flyer />
       <About />
       <Menu />
       <Gallery />
